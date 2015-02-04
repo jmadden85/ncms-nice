6eded821527c0497166af4fac6b60a14b53aec03
jmadden85 <jmadden85@gmail.com>
2015-02-04 18:36:18 -0500

    angular as syntax

--- public/modules/flapper/app.js
+++ public/modules/flapper/app.js
@@ -1,9 +1,24 @@
 var app = angular.module('flapperNews', []);
 
-app.controller('MainCtrl',
-  ['$scope',
-    function ($scope) {
-      $scope.test = 'Testing 1 2 3';
-    }
-  ]
+app.controller('MainCtrl', [function () {
+    var main = this;
+    main.test = 'Testing 1 2 3';
+    main.list = ['a', 'b', 'c', 'd'];
+    main.buttonText = "some button";
+    main.buttonClick = function () {
+      alert(main.buttonText);
+    };
+    main.formSubmit = function () {
+      console.log(this.title);
+      return false;
+    };
+  }]
+);
+
+app.factory('posts', [function () {
+    var o = {
+      posts: []
+    };
+    return o;
+  }]
 );
