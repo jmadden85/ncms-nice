a1fc0ac712adc284b77d0329587d2cd0a384ee9e
jmadden85 <jmadden85@gmail.com>
2015-02-04 19:30:05 -0500

    more updates

--- public/modules/flapper/app.js
+++ public/modules/flapper/app.js
@@ -1,24 +1,50 @@
-var app = angular.module('flapperNews', []);
+var app = angular.module('flapperNews', ['ngRoute']);
 
-app.controller('MainCtrl', [function () {
+app.factory('posts', function () {
+    var service = {};
+    service.posts = [];
+    service.addPost = function (post) {
+      service.posts.push(post);
+    };
+    return service;
+  }
+);
+
+app.controller('TestCtrl', [function () {
+    var test = this;
+    test.text = 'this is our testing page';
+  }]
+);
+
+app.config(function ($routeProvider, $locationProvider) {
+  $routeProvider.
+    when('/test', {
+      templateUrl: 'test.html',
+      controllerAs: 'test',
+      controller: 'TestCtrl'
+    }).
+    otherwise({
+      redirectTo: '/'
+    });
+  $locationProvider.html5Mode({
+    enabled: true,
+    requireBase: false
+  });
+});
+
+app.controller('MainCtrl', ['posts', function (posts) {
     var main = this;
     main.test = 'Testing 1 2 3';
     main.list = ['a', 'b', 'c', 'd'];
     main.buttonText = "some button";
+    main.posts = posts.posts;
+    main.addPosts = posts.addPost;
     main.buttonClick = function () {
       alert(main.buttonText);
     };
     main.formSubmit = function () {
-      console.log(this.title);
+      main.addPosts(this.title);
       return false;
     };
-  }]
-);
-
-app.factory('posts', [function () {
-    var o = {
-      posts: []
-    };
-    return o;
   }]
 );
